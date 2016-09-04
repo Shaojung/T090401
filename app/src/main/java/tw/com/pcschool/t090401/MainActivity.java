805bea0394deb5651575e9c0373d017768dbff9d
Shaojung <shaojung@gmail.com>
2016-09-04 09:59:18 +0800

    Finish

--- app/src/main/java/tw/com/pcschool/t090401/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t090401/MainActivity.java
@@ -5,54 +5,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tv2 = (TextView) findViewById(R.id.textView2);
     }
 
     public void click1(View v)
     {
         MyAsyncTask task = new MyAsyncTask();
-        task.execute(3);
+        task.execute(5);
 
     }
 
+    class MyAsyncTask extends AsyncTask<Integer, Integer, Integer>
+    {
 
-}
-
-class MyAsyncTask extends AsyncTask<Integer, Integer, Integer>
-{
-
-    @Override
-    protected void onProgressUpdate(Integer... values) {
-        super.onProgressUpdate(values);
-
-    }
-
-    @Override
-    protected Integer doInBackground(Integer... params) {
-        int n = params[0];
-        int i;
-        for (i=0;i<n;i++)
-        {
-            try {
-                Thread.sleep(1000);
-                Log.d("TASK", "Sleep:" + i);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tv2.setText(String.valueOf(values[0]));
         }
-        return i;
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            int n = params[0];
+            int i;
+            for (i=0;i<n;i++)
+            {
+                try {
+                    Thread.sleep(1000);
+                    Log.d("TASK", "Sleep:" + i);
+                    publishProgress(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return i;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            Log.d("TASK", "finish:" + integer);
+            tv2.setText("finish:" + integer);
+        }
     }
 
-    @Override
-    protected void onPostExecute(Integer integer) {
-        super.onPostExecute(integer);
-        Log.d("TASK", "finish:" + integer);
-    }
 }
+
